37b570082d5171b012391bf396b0e6dae6b53fdd
Lauravilen <44665204+Lauravilen@users.noreply.github.com>
2022-11-11 17:00:08 +0100

    Updated enemyspawner + related scripts
    Added functionality to the enemyspawner, so it can spawn different types of enemies at different rates.
    Implemented this with Chort and BossChort.

--- Assets/_Scripts/Enemies/EnemySpawner.cs
+++ Assets/_Scripts/Enemies/EnemySpawner.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemySpawner : MonoBehaviour
 {
@@ -8,6 +11,9 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private List<GameObject> spawnPoints = null;
     [SerializeField] private int count = 20;
     [SerializeField] private float minDelay = 0.8f, maxDelay = 1.5f;
+
+    [SerializeField] private List<EnemySpawnData> enemiesToSpawn = new List<EnemySpawnData>();
+    private float[] enemyWeights;
 
     IEnumerator SpawnCoroutine()
     {
@@ -27,11 +33,13 @@ public class EnemySpawner : MonoBehaviour
 
     private void SpawnEnemy(Vector3 spawnPoint)
     {
-        Instantiate(enemyPrefab, spawnPoint, Quaternion.identity);
+        int index = GetRandomWeightedIndex(enemyWeights);
+        Instantiate(enemiesToSpawn[index].enemyPrefab, spawnPoint, Quaternion.identity);
     }
 
     private void Start()
     {
+        enemyWeights = enemiesToSpawn.Select(enemy => enemy.rate).ToArray();
         if (spawnPoints.Count>0)
         {
             foreach (var spawnPoint in spawnPoints)
@@ -42,4 +50,32 @@ public class EnemySpawner : MonoBehaviour
 
         StartCoroutine(SpawnCoroutine());
     }
+
+    private int GetRandomWeightedIndex(float[] enemyWeights)
+    {
+        float sum = 0f;
+        for (int i = 0; i < enemyWeights.Length; i++)
+        {
+            sum += enemyWeights[i];
+        }
+        float randomValue = Random.Range(0, sum);
+        float tempSum = 0;
+        for (int i = 0; i < enemyWeights.Length; i++)
+        {
+            if (randomValue >= tempSum && randomValue < tempSum + enemyWeights[i])
+            {
+                return i;
+            }
+
+            tempSum += enemyWeights[i];
+        }
+        return 0;
+    }
+}
+
+[Serializable]
+public struct EnemySpawnData
+{
+    [Range(0, 1)] public float rate;
+    public GameObject enemyPrefab;
 }
